394be1c0992ca40a683c79307bb342bb61307421
Brandon Trump <brandontrump101@gmail.com>
2023-04-28 16:11:56 +1200

    feat: included TempCart component

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -2,6 +2,7 @@ import { getClient } from "@/apollo-client";
 import CalloutCard from "@/components/CalloutCard";
 import InformationPanel from "@/components/InformationPanel";
 import StatCard from "@/components/StatCard";
+import TempChart from "@/components/TempChart";
 import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
 
 type Props = {
@@ -30,9 +31,10 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
   // console.log(results);
 
   return (
-    <div>
+    <div className="flex flex-col min-h-screen md:flex-row">
       <InformationPanel city={city} lat={lat} long={long} results={results} />
-      <div>
+
+      <div className="flex-1 p-5 lg:p-10">
         <div className="p-5">
           <div className="pb-5">
             <h2 className="text-xl font-bold">Todays Overview</h2>
@@ -86,7 +88,9 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
           </div>
         </div>
         <hr className="mb-5" />
-        <div className="space-y-3"></div>
+        <div className="space-y-3">
+          <TempChart results={results} />
+        </div>
       </div>
     </div>
   );

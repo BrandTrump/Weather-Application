934845f5af8b829f777f34ff0f11ca42aebe45d3
Brandon Trump <brandontrump101@gmail.com>
2023-04-29 23:05:39 +1200

    feat: created a humidity chart that gives the humidity percentage

---
+++ components/HumidityChart.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Card, AreaChart, Title } from "@tremor/react";
+
+type Props = {
+  results: Root;
+};
+
+function HumidityChart({ results }: Props) {
+  const hourly = results?.hourly.time
+    .map((time) =>
+      new Date(time).toLocaleString("en-US", {
+        hour: "numeric",
+        hour12: false,
+      })
+    )
+    .slice(0, 24);
+
+  const data = hourly.map((hour, index) => ({
+    time: Number(hour),
+    "Humidity (%)": results.hourly.precipitation_probability[index],
+  }));
+
+  const dataFormatter = (number: number) => `${number} %`;
+  return (
+    <div>
+      <Card>
+        <Title>Humidity Levels</Title>
+        <AreaChart
+          className="mt-6"
+          data={data}
+          showLegend
+          index="time"
+          categories={["Humidity (%)"]}
+          colors={["teal"]}
+          minValue={0}
+          maxValue={100}
+          valueFormatter={dataFormatter}
+          yAxisWidth={40}
+        />
+      </Card>
+    </div>
+  );
+}
+
+export default HumidityChart;
